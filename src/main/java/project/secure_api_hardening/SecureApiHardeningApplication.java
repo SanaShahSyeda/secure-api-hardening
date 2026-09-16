ea36159fe0c3a5eebe0d74d04839beaa309ad565
Sana Gul <sanashaaah4@gmail.com>
2026-09-16 20:14:12 +0500

    feat: add initial commit for secure-api-hardening

---
+++ src/main/java/project/secure_api_hardening/SecureApiHardeningApplication.java
@@ -0,0 +1,13 @@
+package project.secure_api_hardening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureApiHardeningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureApiHardeningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/secure_api_hardening/SecureApiHardeningApplicationTests.java
@@ -0,0 +1,13 @@
+package project.secure_api_hardening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureApiHardeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
